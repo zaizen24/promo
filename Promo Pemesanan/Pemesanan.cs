9c09c3d2f9263fca7aa4cfb199d1da1645121dcd
zaizen24 <116472858+zaizen24@users.noreply.github.com>
2023-06-22 20:57:07 +0700

    Add files via upload

---
+++ Promo Pemesanan/Pemesanan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Promo_Pemesanan
+{
+    public partial class Pemesanan : Form
+    {
+        public Pemesanan()
+        {
+            InitializeComponent();
+        }
+
+        public string promodigunakan { get; set; }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = promodigunakan;
+        }
+
+        private void Pemesanan_Load(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int textBoxValue))
+            {
+                Form1 form1 = new Form1();
+                form1.tiketdipesan = textBoxValue; // Mengatur nilai properti tiketdipesan di Form 2
+                form1.ShowDialog(); // Menampilkan Form2 sebagai dialog
+                this.Hide(); // Menyembunyikan Form1
+            }
+        }
+    }
+}
